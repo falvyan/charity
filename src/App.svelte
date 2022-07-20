4319caa74e3742fed8caa6cb8acbc1a095f13246
Fahmi Alvyan <nenekkurock@gmail.com>
2022-07-20 11:02:59 +0700

    perbarui footer

--- src/App.svelte
+++ src/App.svelte
@@ -6,7 +6,7 @@
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
-<footer>copyright</footer>
+<footer>coba</footer>
 
 <style>
 	main {
